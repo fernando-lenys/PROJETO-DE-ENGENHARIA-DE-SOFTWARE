7689b4e553c103a3a203b4b51efa58792326f730
Fernando Lenys <fernando@onecommerce.es>
2024-07-11 20:32:02 -0300

    add TaskController routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
  
 
 /*
@@ -20,12 +20,27 @@ Route::get('/', function () {
 });
 
 
+// returns the home page with all tasks
+Route::get('/', TaskController::class .'@index')->name('tasks.index');
+// returns the form for adding a task
+Route::get('/tasks/create', TaskController::class . '@create')->name('tasks.create');
+// adds a post to the database
+Route::post('/tasks', TaskController::class .'@store')->name('tasks.store');
+// returns a page that shows a task
+Route::get('/tasks/{post}', TaskController::class .'@show')->name('tasks.show');
+// returns the form for editing a task
+Route::get('/tasks/{post}/edit', TaskController::class .'@edit')->name('tasks.edit');
+// updates a task
+Route::put('/tasks/{post}', TaskController::class .'@update')->name('tasks.update');
+// deletes a task
+Route::delete('/tasks/{post}', TaskController::class .'@destroy')->name('tasks.destroy');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    
+    Route::get('/dashboard',  TaskController::class .'@index')->name('dashboard');
 });
+
